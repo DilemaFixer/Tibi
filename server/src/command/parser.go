1d492ebc04a05bb8029277ab3f09d38977fc5d93
Illa Shisko <109586143+DilemaFixer@users.noreply.github.com>
2025-07-19 01:00:27 +0200

    bit parsing command to several funcs with checks and error throwing

--- server/src/command/parser.go
+++ server/src/command/parser.go
@@ -1,61 +1,31 @@
 package command
 
 import (
-	"strings"
 	"fmt"
-	"log"
+	"strings"
 )
 
-func ParseToCommand(str string) (*Command , error) {
+func ParseToCommand(str string) (*Command, error) {
+	strings.TrimSpace(str)
+
 	if str == "" || str == " " {
-		return nil , fmt.Errorf("Error parsing command , input is empty")
+		return nil, fmt.Errorf("Error parsing command , input is empty")
 	}
+
 	parts := split(str)
-	
 	command := NewMinimalCommand(parts[0])
-	partsCount := len(parts)
-	if partsCount > 1 {
-		command.Subcommand = parts[1]
+	setSubcommandIfCan(command, parts)
+	err := setSignificancesIfCan(command , parts)
+	if err != nil {
+		return nil , err
 	}
-
-	if (partsCount - 2) != 0 {
-		signsStr := parts[2:]
-		signs := make([]Significance, 0)
-		//TODO: refactor this part of code , move it to new func
-		for _, signStr := range signsStr {
-			signLenght := len(signStr)
-			if strings.HasPrefix(signStr, ".") && signLenght > 1 {
-				signStr = removeCharsFromBegine(signStr, 1)
-				values := strings.Split(signStr, "=")
-				if len(values) != 2 {
-					return nil , fmt.Errorf("Invalid var seter declaration")
-				}
-				sign := NewSignificance(Prop, values[0], values[1])
-				signs = append(signs, sign)
-				continue 
-			}
-
-			if strings.HasPrefix(signStr, "--") && signLenght > 2{
-				signStr = removeCharsFromBegine(signStr, 2)
-				sign := NewSignificance(Flag, signStr, "")	
-				signs = append(signs, sign)
-				continue
-			}
-			
-			sign := NewSignificance(Data, "", signStr)	
-			signs = append(signs, sign)
-		}
-		command.Significances = signs
-	}
-
-	return command , nil
+	return command, nil
 }
 
-func split(str string) []string{
+func split(str string) []string {
 	str = strings.TrimSpace(str)
-	//TODO: check is str is nil or empty | len(str) == 0
 
-	cutPoints := make([]int , 1) // cutPoints[0] == 0 (string start)
+	cutPoints := make([]int, 1) // cutPoints[0] == 0 (string start)
 	var inQuotes bool
 
 	for i, char := range str {
@@ -65,12 +35,12 @@ func split(str string) []string{
 
 		if char == ' ' && !inQuotes {
 			if i > 0 && str[i-1] != ' ' {
-				cutPoints = append(cutPoints , i)
+				cutPoints = append(cutPoints, i)
 			}
 		}
 	}
 
-	cutPoints = append(cutPoints , len(str))
+	cutPoints = append(cutPoints, len(str))
 	var result []string
 	for i := 0; i < len(cutPoints)-1; i++ {
 		start := cutPoints[i]
@@ -86,21 +56,92 @@ func split(str string) []string{
 
 		if start < end {
 			segment := str[start:end]
-			if len(segment) > 2 && segment[0] == '"' && 
-			segment[len(segment)-1] == '"'{
-				segment = segment[1:len(segment)-1]
+			if len(segment) > 2 && segment[0] == '"' &&
+				segment[len(segment)-1] == '"' {
+				segment = segment[1 : len(segment)-1]
 
-				}
-			result = append(result , segment)
+			}
+			result = append(result, segment)
 		}
 	}
 	return result
 }
 
-func removeCharsFromBegine(str string , count int) string {
-	//TODO: check len(str) < count return "" | check is string is empty
 
+func setSubcommandIfCan(command *Command, parts []string) {
+	partsCount := len(parts)
+	if partsCount > 1 {
+		command.Subcommand = parts[1]
+	}
+}
+
+func setSignificancesIfCan(command *Command, parts []string) error {
+	if (len(parts) - 2) == 0 {
+		return nil
+	}
+
+	signsStr := parts[2:]
+	signs := make([]Significance, 0)
+
+	for _, signStr := range signsStr {
+		signLenght := len(signStr)
+
+		if success, err := tryParseVarSeter(&signs, signStr, signLenght); err != nil {
+			return err
+		} else if success || tryParseFlag(&signs, signStr, signLenght) {
+			continue
+		}
+
+		parseData(&signs, signStr)
+	}
+
+	command.Significances = signs
+	return nil
+}
+
+func tryParseVarSeter(signs *[]Significance , str string , strlen int) (bool , error) {
+	if !strings.HasPrefix(str, ".") {
+		return false , nil
+	}
+
+	if strlen <= 1 {
+		return false, fmt.Errorf("Error parsing var seter : invalid format")
+	}
+	
+	str = removeCharsFromBegine(str, 1)
+	values := strings.Split(str, "=")
+	if len(values) != 2 {
+		return false, fmt.Errorf("Error parsing var seter : invalid var seter declaration")
+	}
+	sign := NewSignificance(Prop, values[0], values[1])
+	*signs = append(*signs, sign)
+	return true, nil
+}
+
+func tryParseFlag(signs *[]Significance , str string , strlen int) bool {
+	if !strings.HasPrefix(str, "--") || strlen <= 2 {
+		return false
+	}
+
+	str = removeCharsFromBegine(str, 2)
+	sign := NewSignificance(Flag, str, "")
+	*signs = append(*signs, sign)
+	return true
+}
+
+func parseData(signs *[]Significance , str string) {
+	sign := NewSignificance(Data, "", str)
+	*signs = append(*signs, sign)
+}
+
+func removeCharsFromBegine(str string, count int) string {
+	if count <= 0 {
+		return str
+	}
+
+	if strlen := len(str); strlen < count || strlen == 0 {
+		return ""
+	}
 	result := str[count:]
 	return result
 }
-
