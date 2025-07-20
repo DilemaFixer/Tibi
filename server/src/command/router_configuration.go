af6b970e2d3117ee65c7005fc0777df342fbcd6e
Illa Shisko <109586143+DilemaFixer@users.noreply.github.com>
2025-07-20 15:07:21 +0200

    Bit code by files | Configuration and initi funcs

---
+++ server/src/command/router_configuration.go
@@ -0,0 +1,45 @@
+package command
+
+func (eg EndPointsGroup) AddEndPoint(name string, handler func([]Significance) error) *EndPoint {
+	_, exist := eg.router.way[eg.name]
+
+	if !exist {
+		eg.router.way[eg.name] = make([]*EndPoint, 0)
+	}
+	newEndPoint := NewEndPoint(name, handler, eg)
+	eg.router.way[eg.name] = append(eg.router.way[eg.name], newEndPoint)
+	return newEndPoint
+}
+
+func (eg EndPointsGroup) EndFilling() *Router {
+	return eg.router
+}
+
+func (ep *EndPoint) NewName(newName string) *EndPoint {
+	ep.name = newName
+	return ep
+}
+
+func (ep *EndPoint) SetFlagsEnable(isEnable bool) *EndPoint {
+	ep.isFlagsEnable = isEnable
+	return ep
+}
+
+func (ep *EndPoint) SetVarsEnable(isEnable bool) *EndPoint {
+	ep.isVarsEnable = isEnable
+	return ep
+}
+
+func (ep *EndPoint) AddFlag(flag string) *EndPoint {
+	ep.existFlags = append(ep.existFlags, flag)
+	return ep
+}
+
+func (ep *EndPoint) AddVar(name string, tprop PropType, defaultValue string, info string) *EndPoint {
+	ep.existVars = append(ep.existVars, NewVar(name, tprop, defaultValue != "", defaultValue, info))
+	return ep
+}
+
+func (eg *EndPoint) EndFilling() *EndPointsGroup {
+	return &eg.group
+}
